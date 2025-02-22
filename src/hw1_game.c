86fcad4265f2fd3e13ec5a81f850671cfba451dd
Yusuf Badar <ybadar13@gmail.com>
2025-02-22 22:13:25 +0000

    Part 1b

--- src/hw1_game.c
+++ src/hw1_game.c
@@ -4,6 +4,23 @@
 #include <assert.h>
 
 #include "hw1.h"
+#define MAX_LENGTH 8
+
+extern char board[MAX_LENGTH][MAX_LENGTH];
+extern int top_key[MAX_LENGTH];
+extern int bottom_key[MAX_LENGTH];
+extern int left_key[MAX_LENGTH];
+extern int right_key[MAX_LENGTH];
+
+int initialize_board(const char *initial_state, const char *keys, int size);
+void printBoard(int size);
+int solve(const char *initial_state, const char *keys, int size);
+
+bool isCellEmpty(int row, int col);
+bool checkRowDuplicates(int size);
+bool checkColDuplicates(int size);
+bool checkFilledRow(int size);
+bool checkFilledCol(int size);
 
 int main(int argc, char **argv) {
     assert(argc == 4); //we want a size, board state, and a list of keys
